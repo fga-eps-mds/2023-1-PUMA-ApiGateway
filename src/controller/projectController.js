10bada2703c438f794133b17de862a3b2c2ad705
Luis Taira <luishptaira@gmail.com>
2021-10-27 15:54:26 -0300

    [#26] Trying to fix sonar errors

--- src/controller/projectController.js
+++ src/controller/projectController.js
@@ -1,80 +1,66 @@
 const axios = require('axios');
+const projUrlgetAlocated = `${global.URL_PROJECT}/alocated/`;
+const projUrlgetProject = `${global.URL_PROJECT}/project/`;
+const projUrlputAlocated = `${global.URL_PROJECT}/alocated/status`;
+const projUrlgetAllSubjects = `${global.URL_PROJECT}/subject`;
+const projUrlputProposal = `${global.URL_PROJECT}/proposal/`;
+const projUrlputProposalStatus = `${global.URL_PROJECT}/alocate/`;
 
 module.exports = {
   getAlocated: (body) => {
-    const projUrl = `${global.URL_PROJECT}/alocated/${body.subjectId}`;
     return new Promise((resolve, reject) => {
-      if (projUrl.startsWith(global.URL_PROJECT)) {
-        axios.get(projUrl).then((response) => {
-          resolve(response);
-        }).catch((error) => {
-          console.log('asd\n\n\nfdghfghf\n\n\n');
-          console.log(error);
-          reject(error);
-        });
-      }
+      axios.get(projUrlgetAlocated + body.subjectId).then((response) => {
+        resolve(response);
+      }).catch((error) => {
+        reject(error);
+      });
     });
   },
   getProject: (body) => {
-    const projUrl = `${global.URL_PROJECT}/project/${body.projectId}`;
     return new Promise((resolve, reject) => {
-      if (projUrl.startsWith(global.URL_PROJECT)) {
-        axios.get(projUrl).then((response) => {
-          resolve(response);
-        }).catch((error) => {
-          reject(error);
-        });
-      }
+      axios.get(projUrlgetProject + body.projectId).then((response) => {
+        resolve(response);
+      }).catch((error) => {
+        reject(error);
+      });
     });
   },
   putAlocated: (body) => {
-    const projUrl = `${global.URL_PROJECT}/alocated/status`;
     return new Promise((resolve, reject) => {
-      if (projUrl.startsWith(global.URL_PROJECT)) {
-        axios.put(projUrl, body).then((response) => {
-          resolve(response);
-        }).catch((error) => {
-          reject(error);
-        });
-      }
+      axios.put(projUrlputAlocated, body).then((response) => {
+        resolve(response);
+      }).catch((error) => {
+        reject(error);
+      });
     });
   },
   getAllSubjects: () => {
-    const projUrl = `${global.URL_PROJECT}/subject`;
     return new Promise((resolve, reject) => {
-      if (projUrl.startsWith(global.URL_PROJECT)) {
-        axios.get(projUrl).then((response) => {
-          resolve(response);
-        }).catch((error) => {
-          reject(error);
-        });
-      }
+      axios.get(projUrlgetAllSubjects).then((response) => {
+        resolve(response);
+      }).catch((error) => {
+        reject(error);
+      });
     });
   },
   putProposal: (req) => {
     const projId = req.params.projectId;
-    const projUrl = `${global.URL_PROJECT}/proposal/${projId}`;
     return new Promise((resolve, reject) => {
-      if (projUrl.startsWith(global.URL_PROJECT)) {
-        axios.put(projUrl, req.body).then((response) => {
-          resolve(response);
-        }).catch((error) => {
-          reject(error);
-        });
-      }
+      axios.put(projUrlputProposal + projId, req.body).then((response) => {
+        resolve(response);
+      }).catch((error) => {
+        reject(error);
+      });
     });
   },
   putProposalStatus: (req) => {
     const projId = req.params.projectId;
-    const projUrl = `${global.URL_PROJECT}/alocate/${projId}/status`;
     return new Promise((resolve, reject) => {
-      if (projUrl.startsWith(global.URL_PROJECT)) {
-        axios.put(projUrl, req.body).then((response) => {
-          resolve(response);
-        }).catch((err) => {
-          reject(err);
-        });
-      }
+      axios.put(projUrlputProposalStatus + projId + '/status', req.body).then((response) => {
+        resolve(response);
+      }).catch((err) => {
+        reject(err);
+      });
     });
   },
 };
