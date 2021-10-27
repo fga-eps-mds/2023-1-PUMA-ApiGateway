3c5dcd75b62d183072350de75c965c2b109d2413
Eduardo Lima <eduardolima.df@gmail.com>
2021-10-27 14:58:37 -0300

    HOTFIX setting url_hom for cors

--- src/config/environment.js
+++ src/config/environment.js
@@ -4,10 +4,12 @@ module.exports = {
   configEnv: () => {
     if (process.env.ENVIRONMENT === 'dev') {
       global.URL_USER = `http://${process.env.IP_ADDRESS}:3001`;
+      global.URL_HOM = `http://${process.env.IP_ADDRESS}:8080`;
       global.SECRET = process.env.SECRET;
     } else if (process.env.ENVIRONMENT === 'test') {
       global.URL_GATEWAY = `http://${process.env.IP_ADDRESS}:3004`;
       global.URL_USER = `http://${process.env.IP_ADDRESS}:3001`;
+      global.URL_HOM = `http://${process.env.IP_ADDRESS}:8080`;
       global.SECRET = process.env.SECRET;
     } else if (process.env.ENVIRONMENT === 'prod') {
       global.URL_GATEWAY = `http://${process.env.IP_ADDRESS}:3004`;
