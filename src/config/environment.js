096e4fd392186c5091eacff4ea69fcc66008e524
Mexazonic <brunnohenriq697@gmail.com>
2022-05-06 02:46:55 -0300

    Adição de ambiente hom (homologação) ApiGateway

--- src/config/environment.js
+++ src/config/environment.js
@@ -10,6 +10,12 @@ module.exports = {
       global.SECRET = process.env.SECRET;
     } else if (process.env.ENVIRONMENT === 'test') {
       global.URL_GATEWAY = `http://${process.env.IP_ADDRESS}:3004`;
+      global.URL_USER = `http://${process.env.IP_ADDRESS}:3001`;
+      global.URL_PROJECT = `http://${process.env.IP_ADDRESS}:3000`;
+      global.URL_HOM = `http://${process.env.IP_ADDRESS}:8080`;
+      global.SECRET = process.env.SECRET;
+    } else if (process.env.ENVIRONMENT === 'hom') {
+      global.URL_GATEWAY = `http://${process.env.IP_ADDRESS}:3004`;
       global.URL_USER = `${process.env.URL_USER}`;
       global.URL_PROJECT = `${process.env.URL_PROJECT}`;
       global.URL_ALOCATE = `http://${process.env.URL_ALOCATE}:3002`;
@@ -17,7 +23,7 @@ module.exports = {
       global.SECRET = process.env.SECRET;
     } else if (process.env.ENVIRONMENT === 'prod') {
       global.URL_GATEWAY = `http://${process.env.IP_ADDRESS}:3004`;
-      global.URL_HOM = `http://puma.hml.one`
+      global.URL_HOM = `http://puma.hml.one`;
       global.URL_USER = `http://${process.env.IP_ADDRESS}:3001`;
       global.URL_PROJECT = `http://${process.env.IP_ADDRESS}:3000`;
       global.URL_ALOCATE = `http://${process.env.IP_ADDRESS}:3002`;
