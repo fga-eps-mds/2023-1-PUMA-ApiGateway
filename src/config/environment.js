1e924a3f2651ab69dde86462874bb6fac5e5ca1d
Giovana Vitor Dionisio <giovanadionisio.gd@gmail.com>
2023-01-17 04:14:00 -0300

    Use container name on test urls

--- src/config/environment.js
+++ src/config/environment.js
@@ -9,9 +9,9 @@ module.exports = {
       global.URL_HOM = `http://${process.env.IP_ADDRESS}:8080`;
       global.SECRET = process.env.SECRET;
     } else if (process.env.ENVIRONMENT === 'test') {
-      global.URL_GATEWAY = `http://${process.env.IP_ADDRESS}:3004`;
-      global.URL_USER = `http://${process.env.IP_ADDRESS}:3001`;
-      global.URL_PROJECT = `http://${process.env.IP_ADDRESS}:3000`;
+      global.URL_GATEWAY = `http://api-gateway-test:3004`;
+      global.URL_USER = `http://user-service-test:3001`;
+      global.URL_PROJECT = `http://project-service-test:3000`;
       global.URL_HOM = `http://${process.env.IP_ADDRESS}:8080`;
       global.SECRET = process.env.SECRET;
     } else if (process.env.ENVIRONMENT === 'hom') {
