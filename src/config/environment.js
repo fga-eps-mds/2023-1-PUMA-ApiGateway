5b9d01e24163f0d005f369ef28c3430177adeaa7
Thiago Carvalho <thiago099carvalho@gmail.com>
2023-05-02 17:01:49 -0300

    change hom project service endpoint

--- src/config/environment.js
+++ src/config/environment.js
@@ -9,21 +9,21 @@ module.exports = {
       global.URL_HOM = `http://${process.env.IP_ADDRESS}:8080`;
       global.SECRET = process.env.SECRET;
     } else if (process.env.ENVIRONMENT === 'test') {
-      global.URL_GATEWAY = `http://api-gateway-test:3004`;
-      global.URL_USER = `http://user-service-test:3001`;
-      global.URL_PROJECT = `http://project-service-test:3000`;
+      global.URL_GATEWAY = 'http://api-gateway-test:3004';
+      global.URL_USER = 'http://user-service-test:3001';
+      global.URL_PROJECT = 'http://project-service-test:3000';
       global.URL_HOM = `http://${process.env.IP_ADDRESS}:8080`;
       global.SECRET = process.env.SECRET;
     } else if (process.env.ENVIRONMENT === 'hom') {
       global.URL_GATEWAY = `http://${process.env.IP_ADDRESS}:3004`;
       global.URL_USER = `${process.env.URL_USER}`;
-      global.URL_PROJECT = `${process.env.URL_PROJECT}`;
+      global.URL_PROJECT = 'https://puma-projectservice.herokuapp.com';
       global.URL_ALOCATE = `http://${process.env.URL_ALOCATE}:3002`;
       global.URL_HOM = `http://${process.env.IP_ADDRESS}:8080`;
       global.SECRET = process.env.SECRET;
     } else if (process.env.ENVIRONMENT === 'prod') {
       global.URL_GATEWAY = `http://${process.env.IP_ADDRESS}:3004`;
-      global.URL_HOM = `http://puma.hml.one`;
+      global.URL_HOM = 'http://puma.hml.one';
       global.URL_USER = `http://${process.env.IP_ADDRESS}:3001`;
       global.URL_PROJECT = `http://${process.env.IP_ADDRESS}:3000`;
       global.URL_ALOCATE = `http://${process.env.IP_ADDRESS}:3002`;
