394efe1f6b92212a5d502bc8561a5da08fb0aa48
Thiago Carvalho <thiago099carvalho@gmail.com>
2023-05-24 16:11:39 -0300

    feat: create projectController test cases

--- src/controller/subjectController.js
+++ src/controller/subjectController.js
@@ -1,80 +1,80 @@
 const axios = require('axios');
 
 module.exports = {
-    addSubject: (data) => {
-        const url = `${global.URL_PROJECT}/subject`;
-        const reqBody = data;
-        return new Promise((resolve, reject) => {
-            axios.post(url, reqBody).then((response) => {
-                resolve(response);
-            }).catch((error) => {
-                reject(error);
-            });
-        });
-    },
+  addSubject: (data) => {
+    const url = `${global.URL_PROJECT}/subject`;
+    const reqBody = data;
+    return new Promise((resolve, reject) => {
+      axios.post(url, reqBody).then((response) => {
+        resolve(response);
+      }).catch((error) => {
+        reject(error);
+      });
+    });
+  },
 
-    getSubject: (subjectid) => new Promise((resolve, reject) => {
-        const url = `${global.URL_PROJECT}/subject/${subjectid}`;
-        axios.get(url).then((response) => {
-            resolve(response);
-        });
-    }),
+  getSubject: (subjectid) => new Promise((resolve, reject) => {
+    const url = `${global.URL_PROJECT}/subject/${subjectid}`;
+    axios.get(url).then((response) => {
+      resolve(response);
+    });
+  }),
 
-    getSubjects: () => new Promise((resolve, reject) => {
-        const url = `${global.URL_PROJECT}/subject`;
-        axios.get(url).then((response) => {
-            resolve(response);
-        });
-    }),
+  getSubjects: () => new Promise((resolve, reject) => {
+    const url = `${global.URL_PROJECT}/subject`;
+    axios.get(url).then((response) => {
+      resolve(response);
+    });
+  }),
 
-    updateSubject: (subjectid, data) => {
-        const url = `${global.URL_PROJECT}/subject/${subjectid}`;
-        const reqBody = data;
-        return new Promise((resolve, reject) => {
-            axios.put(url, reqBody).then((response) => {
-                resolve(response);
-            }).catch((error) => {
-                reject(error);
-            });
-        });
-    },
+  updateSubject: (subjectid, data) => {
+    const url = `${global.URL_PROJECT}/subject/${subjectid}`;
+    const reqBody = data;
+    return new Promise((resolve, reject) => {
+      axios.put(url, reqBody).then((response) => {
+        resolve(response);
+      }).catch((error) => {
+        reject(error);
+      });
+    });
+  },
 
-    deleteSubject: (subjectid) => {
-        const url = `${global.URL_PROJECT}/subject/${subjectid}`;
-        return new Promise((resolve, reject) => {
-            axios.delete(url).then((response) => {
-                resolve(response.data);
-            }).catch((error) => {
-                reject(error);
-            });
-        });
-    },
+  deleteSubject: (subjectid) => {
+    const url = `${global.URL_PROJECT}/subject/${subjectid}`;
+    return new Promise((resolve, reject) => {
+      axios.delete(url).then((response) => {
+        resolve(response.data);
+      }).catch((error) => {
+        reject(error);
+      });
+    });
+  },
 
-    getAvailableKeywordsToSubject: () => new Promise((resolve, reject) => {
-        const url = `${global.URL_PROJECT}/subject/keywords`;
-        axios.get(url).then((response) => {
-            resolve(response);
-        });
-    }),
+  getAvailableKeywordsToSubject: () => new Promise((resolve, reject) => {
+    const url = `${global.URL_PROJECT}/subject/keywords`;
+    axios.get(url).then((response) => {
+      resolve(response);
+    });
+  }),
 
-    getProfessors: () => new Promise((resolve, reject) => {
-        const url = `${global.URL_PROJECT}/professors`;
-        axios.get(url).then((response) => {
-            resolve(response);
-        });
-    }),
+  getProfessors: () => new Promise((resolve, reject) => {
+    const url = `${global.URL_PROJECT}/professors`;
+    axios.get(url).then((response) => {
+      resolve(response);
+    });
+  }),
 
-    getSubareas: () => new Promise((resolve, reject) => {
-        const url = `${global.URL_PROJECT}/subareas`;
-        axios.get(url).then((response) => {
-            resolve(response);
-        });
-    }),
+  getSubareas: () => new Promise((resolve, reject) => {
+    const url = `${global.URL_PROJECT}/subareas`;
+    axios.get(url).then((response) => {
+      resolve(response);
+    });
+  }),
 
-    getKnowledgeAreas: () => new Promise((resolve, reject) => {
-        const url = `${global.URL_PROJECT}/knowledgeareas`;
-        axios.get(url).then((response) => {
-            resolve(response);
-        });
-    }),
+  getKnowledgeAreas: () => new Promise((resolve, reject) => {
+    const url = `${global.URL_PROJECT}/knowledgeareas`;
+    axios.get(url).then((response) => {
+      resolve(response);
+    });
+  }),
 };
