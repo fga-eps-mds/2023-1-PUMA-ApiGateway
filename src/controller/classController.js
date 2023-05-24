9336a4596c92096e6264a67278503540d42bdd8b
Thiago Carvalho <thiago099carvalho@gmail.com>
2023-05-24 16:10:36 -0300

    feat: create classController test

--- src/controller/classController.js
+++ src/controller/classController.js
@@ -1,40 +1,41 @@
 const axios = require('axios');
+require('../config/environment').configEnv();
 
 module.exports = {
-    getClass: (classid) => new Promise((resolve, reject) => {
-        const url = `${global.URL_PROJECT}/class/${classid}`;
-        axios.get(url).then((response) => {
-            resolve(response);
-        });
-    }),
+  getClass: (classid) => new Promise((resolve, reject) => {
+    const url = `${global.URL_PROJECT}/class/${classid}`;
+    axios.get(url).then((response) => {
+      resolve(response);
+    });
+  }),
 
-    getClasses: () => new Promise((resolve, reject) => {
-        const url = `${global.URL_PROJECT}/classes`;
-        axios.get(url).then((response) => {
-            resolve(response);
-        });
-    }),
+  getClasses: () => new Promise((resolve, reject) => {
+    const url = `${global.URL_PROJECT}/classes`;
+    axios.get(url).then((response) => {
+      resolve(response);
+    });
+  }),
 
-    updateClass: (classid, data) => {
-        const url = `${global.URL_PROJECT}/class/${classid}`;
-        const reqBody = data;
-        return new Promise((resolve, reject) => {
-            axios.put(url, reqBody).then((response) => {
-                resolve(response);
-            }).catch((error) => {
-                reject(error);
-            });
-        });
-    },
+  updateClass: (classid, data) => {
+    const url = `${global.URL_PROJECT}/class/${classid}`;
+    const reqBody = data;
+    return new Promise((resolve, reject) => {
+      axios.put(url, reqBody).then((response) => {
+        resolve(response);
+      }).catch((error) => {
+        reject(error);
+      });
+    });
+  },
 
-    deleteClass: (classid) => {
-        const url = `${global.URL_PROJECT}/class/${classid}`;
-        return new Promise((resolve, reject) => {
-            axios.delete(url).then((response) => {
-                resolve(response.data);
-            }).catch((error) => {
-                reject(error);
-            });
-        });
-    },
+  deleteClass: (classid) => {
+    const url = `${global.URL_PROJECT}/class/${classid}`;
+    return new Promise((resolve, reject) => {
+      axios.delete(url).then((response) => {
+        resolve(response.data);
+      }).catch((error) => {
+        reject(error);
+      });
+    });
+  },
 };
