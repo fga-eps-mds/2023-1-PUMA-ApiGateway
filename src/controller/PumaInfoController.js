dec674f74276e868bc3fbc2076ea39918f672abc
Thiago Carvalho <thiago099carvalho@gmail.com>
2023-05-26 19:09:10 -0300

    fix: update puma info controller
    Co-authored-by: Rafaelltm <rafaelltm10@hotmail.com>
    Co-authored-by: abner423 <abner.f.c.r@hotmail.com>

--- src/controller/PumaInfoController.js
+++ src/controller/PumaInfoController.js
@@ -2,25 +2,24 @@ const axios = require('axios');
 
 module.exports = {
 
-    getPumaInfo: () => new Promise((resolve, reject) => {
-        console.log("teste");
-        const url = `${global.URL_PROJECT}/pumaInfo`;
-        axios.get(url).then((response) => {
-            resolve(response);
-        }).catch((e)=>{
-            console.log(e);
-        })
-    }),
+  getPumaInfo: () => new Promise((resolve, reject) => {
+    const url = `${global.URL_PROJECT}/pumaInfo`;
+    axios.get(url).then((response) => {
+      resolve(response);
+    }).catch((e) => {
+      reject(e);
+    });
+  }),
 
-    updatePumaInfo: (data) => {
-        const url = `${global.URL_PROJECT}/pumaInfo`;
-        const reqBody = data;
-        return new Promise((resolve, reject) => {
-            axios.put(url, reqBody).then((response) => {
-                resolve(response);
-            }).catch((error) => {
-                reject(error);
-            });
-        });
-    },
+  updatePumaInfo: (data) => {
+    const url = `${global.URL_PROJECT}/pumaInfo`;
+    const reqBody = data;
+    return new Promise((resolve, reject) => {
+      axios.put(url, reqBody).then((response) => {
+        resolve(response);
+      }).catch((error) => {
+        reject(error);
+      });
+    });
+  },
 };
