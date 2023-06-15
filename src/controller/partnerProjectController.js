682ca4e2cd21d554a26dcf41f16d6031bc09a347
Thiago Carvalho <thiago099carvalho@gmail.com>
2023-06-14 23:01:34 -0300

    feat: add partnerProject controller/route

--- src/controller/partnerProjectController.js
+++ src/controller/partnerProjectController.js
@@ -11,6 +11,33 @@ module.exports = {
     });
   }),
 
+  getProject: (id) => new Promise((resolve, reject) => {
+    const url = `${global.URL_PROJECT}/partnerProject/${id}`;
+    axios.get(url).then((response) => {
+      resolve(response);
+    }).catch((e) => {
+      reject(e);
+    });
+  }),
+
+  updateProject: (id, data) => new Promise((resolve, reject) => {
+    const url = `${global.URL_PROJECT}/partnerProject/${id}`;
+    axios.put(url, data).then((response) => {
+      resolve(response);
+    }).catch((e) => {
+      reject(e);
+    });
+  }),
+
+  deleteProject: (id) => new Promise((resolve, reject) => {
+    const url = `${global.URL_PROJECT}/partnerProject/${id}`;
+    axios.delete(url).then((response) => {
+      resolve(response);
+    }).catch((e) => {
+      reject(e);
+    });
+  }),
+
   createProject: (data) => {
     const url = `${global.URL_PROJECT}/partnerProject`;
     const reqBody = data;
--- src/routes/partnerProjectRouter.js
+++ src/routes/partnerProjectRouter.js
@@ -12,6 +12,33 @@ router.get('/', (req, res) => {
   });
 });
 
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  partnerProjectController.getProject(id).then((response) => {
+    res.status(200).json(response.data);
+  }).catch((error) => {
+    res.status(400).json(error);
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  partnerProjectController.deleteProject(id).then((response) => {
+    res.status(200).json(response.data);
+  }).catch((error) => {
+    res.status(400).json(error);
+  });
+});
+
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  partnerProjectController.updateProject(id, req.body).then((response) => {
+    res.status(200).json(response.data);
+  }).catch((error) => {
+    res.status(400).json(error);
+  });
+});
+
 router.post('/', (req, res) => {
   partnerProjectController.createProject(req.body).then((response) => {
     res.status(200).json(response.data);
